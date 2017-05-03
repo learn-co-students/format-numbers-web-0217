15f04d8cb8efefba5aee8e21dce46ddc4148e2ef
github username <github email address>
2017-05-03 18:50:40 -0400

    Done.

--- format_number.rb
+++ format_number.rb
@@ -1,3 +1,15 @@
 def separate_with_comma(num)
-  # code goes here
+  if num.to_s.length <= 3
+    num.to_s
+  else
+    insertion_points = [2, 5, 8, 11, 14, 17]
+    reversed_num_array = num.to_s.reverse.split("")
+    reversed_num_array.each_with_index.map do |number, index|
+      if (insertion_points.include?(index)) && index != reversed_num_array.count - 1
+        number + ","
+      else
+        number
+      end
+    end.join().reverse
+  end
 end
