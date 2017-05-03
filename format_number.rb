29a06143dc78c8a0978f9a4f7b6923a6f9beb089
dylanito2 <dylanito2@gmail.com>
2017-05-03 17:20:46 -0400

    Done.

--- format_number.rb
+++ format_number.rb
@@ -1,3 +1,15 @@
 def separate_with_comma(num)
-  # code goes here
+  if num.to_s.length <= 3
+    return num.to_s
+  else
+    num = num.to_s.reverse
+    map = num.chars.map.with_index do |int, index|
+      if (index + 1) % 3 == 0 && index != num.length - 1
+        int + ','
+      else
+        int
+      end
+    end
+  end
+  map.join('').reverse
 end
