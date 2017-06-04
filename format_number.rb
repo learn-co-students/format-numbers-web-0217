04ce19b6b3565c760fa87f6aa5960b1001e22e00
fakebenjay <fakebenjay@gmail.com>
2017-06-04 19:14:01 -0400

    Done.

--- format_number.rb
+++ format_number.rb
@@ -1,3 +1,18 @@
 def separate_with_comma(num)
-  # code goes here
+  array = num.to_s.split('').reverse
+  indices = (1..array.length).to_a
+  counter = 0
+
+  indices.each do |i|
+    if i % 3 == 0
+      array.insert(i + counter, ",")
+      counter += 1
+    end
+  end
+
+  if array[-1] == ','
+    array[-1] = ''
+  end
+
+  array.reverse.join
 end
